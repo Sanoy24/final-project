1cdc46ab55a972bc6f3ed0fa6851c2848a82c35e
Yonas Mekonnen <myonas886@gmail.com>
2024-04-12 14:26:51 +0300

    initial commit

---
+++ src/app/login/page.jsx
@@ -0,0 +1,16 @@
+import LoginForm from "../components/login-form";
+
+export default function LoginPage() {
+  return (
+    <main className="flex items-center justify-center md:h-screen">
+      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
+        <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
+          <h1 className="text-white text-xl justify-center content-center object-center pl-16">
+            Chest X-Ray
+          </h1>
+        </div>
+        <LoginForm />
+      </div>
+    </main>
+  );
+}
